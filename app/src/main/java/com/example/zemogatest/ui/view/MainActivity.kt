04233a98a0879a76d828f3779e2af26953c5be6d
Leo <leo_svxy@hotmail.com>
2023-04-08 18:13:39 -0500

    setting up environment

--- app/src/main/java/com/example/zemogatest/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/zemogatest/ui/view/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.zemogatest
+package com.example.zemogatest.ui.view
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
@@ -9,6 +9,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import com.example.zemogatest.R
 import com.example.zemogatest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
